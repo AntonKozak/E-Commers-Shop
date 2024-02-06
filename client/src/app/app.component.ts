d88433befaac9e22481f5424ec1f9e6dbcd0f4cc
Ant <Kozakanton@i.ua>
2024-02-06 19:51:30 +0100

    added product card 91

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,30 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/products';
-import { Pagination } from './shared/models/pagination';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  
-  products: Product[] = [];
+export class AppComponent {
 
-  private http = inject(HttpClient);
-
-  ngOnInit(): void {
-    this.getValues();
-  }
-
-  getValues() {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/Products').subscribe({
-      next: response => this.products = response.data,
-      error: (error) => console.log(error),
-      complete: () => {
-        console.log('Complete');
-      },
-    });
-  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  getProducts() {
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'Products');
+  }
 }
