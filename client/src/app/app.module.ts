9481130f84a0cbebea349899971a214d4c1dc351
Ant <Kozakanton@i.ua>
2024-02-07 20:22:12 +0100

    section 11 Handling errors and trouble shooting them, 122

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/inteceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,9 @@ import { HomeModule } from './home/home.module';
   exports:[
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
