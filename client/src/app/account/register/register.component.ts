2accf32d7e4fb82948a6dc2966063522eb214356
Ant <Kozakanton@i.ua>
2024-02-10 20:55:57 +0100

    Added improvment of validations and some loading indicats 203

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
+import { debounce, debounceTime, finalize, map, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  errors: string[] |null = null;
+
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
+
+  complexPassword = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$";
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email], [this.validateEmailNotTaken()]],
+    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]]
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+      error: (error) => this.errors = error.errors
+    })
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn{
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(700),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkEmailExists(control.value).pipe(
+        map(res => res ? {emailExists: true} : null),
+        finalize(() => control.markAsTouched())
+      );
+        })
+      )
+      
+    }
+  }
 
 }
--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -1,23 +1,25 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable, delay, finalize } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { BusyService } from '../services/busy.service';
+import { Injectable } from '@angular/core';
+import { delay, finalize } from 'rxjs/operators';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
+    constructor(private busyService: BusyService) {}
 
-  constructor(private busyService: BusyService) {}
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      // Add loading spinner here
-      delay(400),
-      finalize(() => {this.busyService.idle()})
-    );
-  }
+        if (req.url.includes('emailexists')) {
+            return next.handle(req);
+        }
+
+        this.busyService.busy();
+        return next.handle(req).pipe(
+          delay(1000),
+            finalize(() => {
+                this.busyService.idle();
+            })
+        );
+    }
 }
