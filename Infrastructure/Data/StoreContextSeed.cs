5992bbe1dfe0fc2318e9d0ecd2e7faebd13656f4
Ant <Kozakanton@i.ua>
2024-02-18 10:58:49 +0100

    The last before pupblishing on Linux 291

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.Json;
 using Core.Entities;
 using Core.Entities.OrderAggregate;
@@ -8,25 +9,26 @@ public class StoreContextSeed
 {
     public static async Task SeedAsync(StoreContext context)
     {
+        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         //it is #1
         if(!context.ProductBrands.Any())
         {
-            // going to API and getting the data, htats why we are going ../Infrastructure/Data...
-            var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+            // going to API and getting the data, htats why we are goingpath + @"/Data...
+            var brandsData = File.ReadAllText(path + @"/Data/SeedData/brands.json");
             var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
             context.ProductBrands.AddRange(brands);
         }
         //it is #2
         if(!context.ProductTypes.Any())
         {
-            var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+            var typesData = File.ReadAllText(path + @"/Data/SeedData/types.json");
             var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
             context.ProductTypes.AddRange(types);
         }
         //it is #3
         if(!context.Products.Any())
         {
-            var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+            var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
             var products = JsonSerializer.Deserialize<List<Product>>(productsData);
             context.Products.AddRange(products);
         }
@@ -36,7 +38,7 @@ public class StoreContextSeed
 
         if(!context.DeliveryMethods.Any())
         {
-            var deliveryData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+            var deliveryData = File.ReadAllText(path + @"/Data/SeedData/delivery.json");
             var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryData);
             context.DeliveryMethods.AddRange(methods);
         }
